5f5827abf3f2b8c3046c423158d115510f98d456
Lenz Weber <mail@lenzw.de>
2017-08-28 18:49:24 +0200

    fix typings

--- types/declarations.d.ts
+++ types/declarations.d.ts
@@ -1,3 +1,9 @@
+// material-ui seems to have some problems - referencing it here solves that
+// see https://github.com/callemall/material-ui/issues/7853
+/// <reference types="@types/material-ui" />
+// web-ext-types is not a @types repo and needs to be referenced here
+/// <reference types="web-ext-types/global" />
+
 import {ExecutionContext} from "../src/Decorators/ExecuteInContext";
 
 declare global {
