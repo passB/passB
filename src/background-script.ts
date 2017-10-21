7f8eca44db2f01500cac2d33cbb1616bc431186d
Lenz Weber <mail@lenzw.de>
2017-10-22 00:58:45 +0200

    makes @executeInCorrectContext easier to use
    * annotating @Reflect.metadata('executionContext', 'background') is not
    required any more.
    * instead of setting window.executionContext, setExecutionContext
    and getExecutionContext can be used
    * class instances should act on the value of executionContext that was
    set at instance creation instead of current executionContext (should
    allow for better testing within the same page)

--- src/background-script.ts
+++ src/background-script.ts
@@ -1,6 +1,7 @@
-window.executionContext = 'background';
-
 import {Container} from 'typedi';
+import {setExecutionContext} from 'Decorators/ExecuteInContext';
 import {PassB} from './PassB';
+
+setExecutionContext('background');
 
 Container.get(PassB).initialize();
